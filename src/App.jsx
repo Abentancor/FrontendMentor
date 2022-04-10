b40f184962ac91ade79b6e2c8db70e9683b32855
Abentancor <Angelbentancor@gmail.com>
2022-04-10 14:59:17 -0300

    instagram border color

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import IconDown from './assets/images/icon-down.svg'
 
 const SocialCard = ({IconSocial, Icon, BorderTop, Follows, ColortextToday='text-green-500', FollowsToday}) =>{
   return (
-      <article className={`bg-[#F0F3FA] rounded-md border-t-8 ${BorderTop} p-8 text-center hover:brightness-90 cursor-pointer transition ease-in delay-75`}>
+      <article className={`bg-[#F0F3FA] rounded-md ${BorderTop} p-8 text-center hover:brightness-90 cursor-pointer transition ease-in delay-75`}>
         <div className='flex justify-center items-center'>
           <img src={IconSocial}alt="IconFacebook" className='mr-2'/>
           <p className='font-semibold text-slate-700'>@lorem</p>
@@ -32,10 +32,10 @@ const App = () => {
 
       </div>
       <section className='grid gap-3 mx-4  md:grid-cols-4 :grid-cols-2'>
-        <SocialCard IconSocial={IconFacebook} Icon={IconUp} BorderTop={'border-blue-600'} Follows='1987' FollowsToday='12' />
-        <SocialCard IconSocial={IconTwitter} Icon={IconUp} BorderTop={'border-sky-500'}  Follows='1044' FollowsToday='99' />
-        <SocialCard IconSocial={IconInstagram} Icon={IconUp} BorderTop={'border-orange-500'}  Follows='11k' FollowsToday='1099'/>
-        <SocialCard IconSocial={IconYoutube} Icon={IconDown} BorderTop={'border-red-500'}  Follows='8239' FollowsToday='144' ColortextToday='text-red-500' />
+        <SocialCard IconSocial={IconFacebook} Icon={IconUp} BorderTop={'border-t-8 border-blue-600'} Follows='1987' FollowsToday='12' />
+        <SocialCard IconSocial={IconTwitter} Icon={IconUp} BorderTop={'border-t-8 border-sky-500'}  Follows='1044' FollowsToday='99' />
+        <SocialCard IconSocial={IconInstagram} Icon={IconUp} BorderTop={'relative before:absolute before:inset-0 before:h-2 before:rounded-t-md before:bg-gradient-to-r before:from-[#FDB55D] before:to-[#DE428B]'}  Follows='11k' FollowsToday='1099'/>
+        <SocialCard IconSocial={IconYoutube} Icon={IconDown} BorderTop={'border-t-8 border-red-500'}  Follows='8239' FollowsToday='144' ColortextToday='text-red-500' />
       </section>
     </div>
   )
